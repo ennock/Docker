67b7eeb0a0bd0c2dfac0af4dce239a888955e4c0
Wilberforce Waswa <46634022+ennock@users.noreply.github.com>
2024-03-06 23:54:31 +0300

    Add files via upload

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,24 @@
-import sys 
+services:
+  postgres:
+    image: postgres:13
+    environment:
+      POSTGRES_USER: airflow
+      POSTGRES_PASSWORD: airflow
+      POSTGRES_DB: airflow
+    volumes:
+      - postgres-db-volume:/var/lib/postgresql/data
+    healthcheck:
+      test: ["CMD", "pg_isready", "-U", "airflow"]
+      interval: 5s
+      retries: 5
+    restart: always
 
-import pandas as pd
 
+docker run -it \
+   -e POSTGRES_USER="root" \
+   -e POSTGRES_PASSWORD="root" \
+   -e POSTGRES_DB="ny_tax" \
+   -v ny_taxi_postgres_data:/var/lib/postgresql/data \
+   -p 5432:5432
 
-print(sys.argv)
-
-day = sys.argv[1]
-
-#comments
-print("job finished successfully")
+    postgres:13 \ 
